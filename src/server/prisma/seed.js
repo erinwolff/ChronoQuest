6986e7c7671546e984d1d221298e4667b53e3bec
Erin Wolff <erin_wolff@outlook.com>
2023-11-21 12:50:38 -0800

    seeded the database with a test user and one game

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -1,20 +1,19 @@
 const prisma = require("../prisma");
 
-/** Seeds the database with a user and some tasks */
+/** Seeds the database with a test user and one game */
 const seed = async () => {
-  await prisma.user.upsert({
-    where: {
-      username: "foo",
-    },
-    update: {},
-    create: {
+  await prisma.user.create({
+    data: {
       username: "foo",
       password: "bar",
-      tasks: {
+      games: {
         create: [
-          { description: "task 1" },
-          { description: "task 2" },
-          { description: "task 3" },
+          {
+            title: "Farthest Frontier",
+            time: "29 Hours",
+            imageUrl: "https://static.pressakey.de/gfxgames/boxart/s/Farthest-Frontier-7045-1610445484.jpg",
+            review: "If you are like me and are looking for a new Banished, look no further! This satisfies the colony sim itch like no other."
+          },
         ],
       },
     },
