24ac136c4ebe1d92db2b1227e74b30abca226d17
Erin Wolff <erin_wolff@outlook.com>
2023-11-22 08:02:38 -0800

    API routes to GET all games posted by logged-in user, POST new game

--- src/server/api/profile.js
+++ src/server/api/profile.js
@@ -3,3 +3,39 @@ const prisma = require("../prisma");
 
 const router = require("express").Router();
 module.exports = router;
+
+// GETs all game posts made by logged-in user
+router.get("/", async (req, res, next) => {
+  try {
+    const games = await prisma.game.findMany({
+      where: { userId: res.locals.user.id },
+    });
+    const responseData = {
+      games: games
+    };
+    res.json(responseData);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POSTs new game while logged in as current user
+router.post("/", async (req, res, next) => {
+  try {
+    const { gameTitle, gameTime, gameImage, gameReview } = req.body;
+    if (!gameTitle || !gameTime) {
+      throw new ServerError(400, "Must provide game title & game time.");
+    }
+    const newGame = await prisma.game.create({
+      data: {
+        title: gameTitle, time: gameTime, imageUrl: gameImage, review: gameReview,
+        user: {
+          connct: { id: res.locals.user.id }
+        }
+      },
+    });
+    res.json(newGame);
+  } catch (err) {
+    next(err);
+  }
+});
