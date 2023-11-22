2d0ccb6c77d5a12e7d883e3269fbaf01aea00ab8
Erin Wolff <erin_wolff@outlook.com>
2023-11-22 07:36:38 -0800

    api request to get all game posts made by users for home page

--- src/server/api/home.js
+++ src/server/api/home.js
@@ -3,3 +3,13 @@ const prisma = require("../prisma");
 
 const router = require("express").Router();
 module.exports = router;
+
+// GETs all the game posts made by all users
+router.get("/", async(req, res, next)=> {
+  try{
+    const result = await prisma.game.findMany();
+    res.json(result);
+  } catch (err){
+    next(err);
+  }
+});
