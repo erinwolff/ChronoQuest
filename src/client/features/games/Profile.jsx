a96bc5afea8b948bf6f7a68495ab63a00df03031
Erin Wolff <erin_wolff@outlook.com>
2023-11-27 11:33:44 -0800

    added form to profile to make a new game post

--- src/client/features/games/Profile.jsx
+++ src/client/features/games/Profile.jsx
@@ -1,27 +1,77 @@
-import { useGetUserGamesQuery } from "./gameSlice";
-import {useSelector} from "react-redux";
-import {selectToken} from "../auth/authSlice"
-import { Link } from 'react-router-dom'
+import { useGetUserGamesQuery, useCreateGameMutation } from "./gameSlice";
+import { useSelector } from "react-redux";
+import { selectToken } from "../auth/authSlice"
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export const GamesCard = ({game}) => {
+export const GamesCard = ({ game }) => {
   return (
     <>
-    <ul className="game-card">
-      <section>
-        <li className="game">
-          <p className="game-text">
-            {game.title}
-            <br />
-            <img src={game.imageUrl} />
-            <br />
-            {game.time}
-            <br />
-            <Link to={`/details/${game.id}`}> Review </Link>
-          </p>
-        </li>
-      </section>
-    </ul>
-  </>
+      <ul className="game-card">
+        <section>
+          <li className="game">
+            <p className="game-text">
+              {game.title}
+              <br />
+              <img src={game.imageUrl} />
+              <br />
+              {game.time}
+              <br />
+              <Link to={`/details/${game.id}`}> Review </Link>
+            </p>
+          </li>
+        </section>
+      </ul>
+    </>
+  )
+}
+
+export const GameForm = () => {
+  const [gameTitle, setGameTitle] = useState("");
+  const [gameTime, setGameTime] = useState("");
+  const [gameImage, setGameImage] = useState("");
+  const [gameReview, setGameReview] = useState("");
+  const [newGame] = useCreateGameMutation();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await newGame({ gameTitle, gameTime, gameImage, gameReview });
+    setGameTitle("");
+    setGameTime("");
+    setGameImage("");
+    setGameReview("");
+  };
+
+  return (
+    <>
+      <form className="add-game-form" onSubmit={handleSubmit}>
+        <input required
+          type="text"
+          placeholder="Title"
+          value={gameTitle}
+          onChange={(e) => setGameTitle(e.target.value)}
+        />
+        <input required
+          type="text"
+          placeholder="Time to beat"
+          value={gameTime}
+          onChange={(e) => setGameTime(e.target.value)}
+        />
+        <input 
+          type="text"
+          placeholder="Image URL"
+          value={gameImage}
+          onChange={(e) => setGameImage(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Review"
+          value={gameReview}
+          onChange={(e) => setGameReview(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+    </>
   )
 }
 
@@ -29,11 +79,11 @@ export const GamesCard = ({game}) => {
 
 export default function Profile() {
   const token = useSelector(selectToken);
-  const {data, isLoading} = useGetUserGamesQuery();
+  const { data, isLoading } = useGetUserGamesQuery();
   const games = data?.games || [];
   const username = data?.username || "";
 
-  if(!token){
+  if (!token) {
     return <p>You must be logged in to view your profile.</p>
   }
 
@@ -44,10 +94,16 @@ export default function Profile() {
       {games && (
         <ul>
           {games?.map((game) => (
-            <GamesCard key = {game.id} game={game}/>
+            <GamesCard key={game.id} game={game} />
           ))}
         </ul>
       )}
+      <br />
+      <br />
+      <h4>Finished a game?</h4>
+      <br />
+      <h4>Add it to the list.</h4>
+      <GameForm />
     </>
   );
 }
