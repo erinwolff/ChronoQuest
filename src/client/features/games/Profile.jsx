008df1c8fe9d14b07dd404ab8e877b0dc44c6ca6
Erin Wolff <erin_wolff@outlook.com>
2023-11-27 11:04:48 -0800

    created boilerplate for Profile component to show all games posted by logged-in user

--- src/client/features/games/Profile.jsx
+++ src/client/features/games/Profile.jsx
@@ -1,9 +1,53 @@
-export default function Profile() {
+import { useGetUserGamesQuery } from "./gameSlice";
+import {useSelector} from "react-redux";
+import {selectToken} from "../auth/authSlice"
+import { Link } from 'react-router-dom'
 
+export const GamesCard = ({game}) => {
+  return (
+    <>
+    <ul className="game-card">
+      <section>
+        <li className="game">
+          <p className="game-text">
+            {game.title}
+            <br />
+            <img src={game.imageUrl} />
+            <br />
+            {game.time}
+            <br />
+            <Link to={`/details/${game.id}`}> Review </Link>
+          </p>
+        </li>
+      </section>
+    </ul>
+  </>
+  )
+}
+
+
+
+export default function Profile() {
+  const token = useSelector(selectToken);
+  const {data, isLoading} = useGetUserGamesQuery();
+  const games = data?.games || [];
+  const username = data?.username || "";
+
+  if(!token){
+    return <p>You must be logged in to view your profile.</p>
+  }
 
   return (
     <>
-      <h1>Profile Page</h1>
+      <h1>{username}'s Games</h1>
+      <br />
+      {games && (
+        <ul>
+          {games?.map((game) => (
+            <GamesCard key = {game.id} game={game}/>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
