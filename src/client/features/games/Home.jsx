3e9362b72ce3f8a72d8d6b0564adac1cd7bbf662
Erin Wolff <erin_wolff@outlook.com>
2023-11-27 08:49:39 -0800

    added search bar to filter games on Home Page by game title or game time to complete

--- src/client/features/games/Home.jsx
+++ src/client/features/games/Home.jsx
@@ -1,5 +1,6 @@
 import { useGetAllGamesQuery } from "./gameSlice";
 import { Link } from 'react-router-dom'
+import { useState } from "react";
 
 
 export const GameCard = ({ game }) => {
@@ -28,25 +29,43 @@ export const GameCard = ({ game }) => {
 export default function Home() {
   const { data: games, isLoading } = useGetAllGamesQuery();
 
+  const [filteredGame, setFilteredGame] = useState("");
+  const filteredGames = games?.filter((g) => (g.title.toLowerCase().includes(filteredGame.toLowerCase()) || g.time.toLowerCase().includes(filteredGame.toLowerCase())));
+
+
   return (
-    <div className="games">
-      <h5>Join the party!</h5>
-      <h6>Create a profile to track your games & write reviews.</h6>
-      <br />
-      <h5>Beat the eternal quest of time!</h5>
-      <h6>Wondering about the time commitment for that next game? Search the collection for detailed information, provided by users, for users! </h6>
-      <br />
-      <h5>See what the world is playing:</h5>
-      <br />
-      {isLoading && <p>Loading games...</p>}
-      {games && (
-        <ul>
-          {games.map((game) => (
-            <GameCard key={game.id} game={game} />
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <div className="search-bar">
+        <input type="text" value={filteredGame} onChange={(e) => setFilteredGame(e.target.value)} placeholder="Search" />
+      </div>
+
+      <div className="games">
+        <h5>Join the party!</h5>
+        <h6>Create a profile to track your games & write reviews.</h6>
+        <br />
+        <h5>Beat the eternal quest of time!</h5>
+        <h6>Wondering about the time commitment for that next game? Search the collection for detailed information, provided by users, for users! </h6>
+        <br />
+        <h5>See what the world is playing:</h5>
+        <br />
+        {isLoading && <p>Loading games...</p>}
+        {filteredGame ? (
+          <ul className="search">
+            {
+              filteredGames?.map((game) => (
+                <GameCard key={game.id} game={game} />
+              ))
+            }
+          </ul>
+        ) : (
+          <ul>
+            {games?.map((game) => (
+              <GameCard key={game.id} game={game} />
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 }
 
