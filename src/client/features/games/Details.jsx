bded6808e17cdfec2942855c61a25c701202e9a9
Erin Wolff <erin_wolff@outlook.com>
2023-11-27 10:42:27 -0800

    created boilerplate for Details page to display a specific game by game id

--- src/client/features/games/Details.jsx
+++ src/client/features/games/Details.jsx
@@ -1,10 +1,46 @@
+import { useGetGameByIdQuery, useDeleteGameMutation } from "./gameSlice";
+import { useParams, useNavigate } from "react-router-dom"
+
+
 export default function Details() {
+  const { id } = useParams();
+  const { data, isLoading } = useGetGameByIdQuery(id);
+  const game = data?.game || {};
+  const username = data?.username || "";
+  const playtime = data?.game.time || "";
+  const review = data?.game.review || "";
 
-
-  return (
+  // Handle delete function
+  const navigate = useNavigate();
+  const [deleteGame] = useDeleteGameMutation();
+  const handleDelete = () => {
+    deleteGame(game.id);
+    navigate("/profile");
+  }
+ 
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (game && (
     <>
-      <h1>Details Page</h1>
+      <main className="game-details">
+        <h3>{game.title}</h3>
+        <h4>Posted by: {username}</h4>
+        <br />
+        <img src={game.imageUrl} />
+        <br />
+        <h4>{username}'s Playtime:</h4>
+        <br />
+        {playtime}
+        <br />
+        <br/>
+        <h4>{username}'s Review:</h4>
+        <br />
+        {review}
+      </main>
+      <button className="deleteButton" onClick={handleDelete}>DELETE</button>
     </>
-  );
+  ))
+
+
 }
 
