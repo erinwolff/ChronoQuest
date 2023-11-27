a3f95b42a9f98ab3cef7b1d06280c63099f44a46
Erin Wolff <erin_wolff@outlook.com>
2023-11-27 08:19:12 -0800

    created home card to display games posted by all users

--- src/client/features/games/Home.jsx
+++ src/client/features/games/Home.jsx
@@ -1,10 +1,43 @@
-export default function Home() {
+import { useGetAllGamesQuery } from "./gameSlice";
 
 
+export const HomeCard = ({ game }) => {
   return (
     <>
-      <h1>Home Page</h1>
+      <ul className="game-card">
+        <section>
+          <li className="game">
+            <p className="game-text">
+              {game.title}
+              <br/>
+              <img src={game.imageUrl}/>
+              <br/>
+              {game.time}
+              <br/>
+            </p>
+          </li>
+        </section>
+      </ul>
     </>
+  )
+}
+
+
+export default function Home() {
+  const { data: games, isLoading } = useGetAllGamesQuery();
+
+  return (
+    <div className="games">
+      <h1>See what the world is playing:</h1>
+      {isLoading && <p>Loading games...</p>}
+      {games && (
+        <ul>
+          {games.map((game) => (
+            <HomeCard key={game.id} game={game} />
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
 
