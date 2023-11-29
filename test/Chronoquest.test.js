488172bc1d67ba68421ab33b5788d08219fdae50
Erin Wolff <erin_wolff@outlook.com>
2023-11-29 10:16:59 -0800

    updated format

--- test/Chronoquest.test.js
+++ test/Chronoquest.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from "../src/client/store"
 import Home from "../src/client/features/games/Home"
 
