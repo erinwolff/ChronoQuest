6aa76b86e808aa1c171823d66e2675bf9fc916f7
Erin Wolff <erin_wolff@outlook.com>
2023-11-30 10:35:12 -0800

    added icons to navbar for navigation and updated their styling

--- src/client/layout/Navbar.jsx
+++ src/client/layout/Navbar.jsx
@@ -2,6 +2,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
 import { logout, selectToken } from "../features/auth/authSlice";
 import clockLogo from "../assets/clock.gif"
+import homeIcon from "../assets/home.png"
+import loginIcon from "../assets/login.png"
+import logoutIcon from "../assets/logout.png"
+import profileIcon from "../assets/user.png"
 
 import "./Navbar.less";
 
@@ -22,19 +26,34 @@ export default function Navbar() {
 
   return (
     <nav className="top">
-      <img className="clockLogo" src={clockLogo}/><h1>ChronoQuest</h1>
+      <img className="clockLogo" src={clockLogo} /><h1>ChronoQuest</h1>
       <menu>
         {token ? (
-          <li>
-            <NavLink to="/home">Home</NavLink>
-            <NavLink to="/profile">Profile</NavLink>
-            <a onClick={handleLogout}>Log Out</a>
-          </li>
+          <>
+            <div className="navIconsContainer">
+              <NavLink to="/home"><img className="navIcons" src={homeIcon} /></NavLink>
+              <p>Home</p>
+            </div>
+            <div className="navIconsContainer">
+              <NavLink to="/profile"><img className="navIcons" src={profileIcon} /></NavLink>
+              <p>Profile</p>
+            </div>
+            <div className="navIconsContainer">
+              <a onClick={handleLogout}><img className="navIcons" src={logoutIcon} /></a>
+              <p>Logout</p>
+            </div>
+          </>
         ) : (
-          <li>
-            <NavLink to="/home">Home</NavLink>
-            <NavLink to="/login">Log In</NavLink>
-          </li>
+          <>
+            <div className="navIconsContainer">
+              <NavLink to="/home"><img className="navIcons" src={homeIcon} /></NavLink>
+              <p>Home</p>
+            </div>
+            <div className="navIconsContainer">
+              <NavLink to="/login"><img className="navIcons" src={loginIcon} /></NavLink>
+              <p>Login</p>
+            </div>
+          </>
         )}
       </menu>
     </nav>
